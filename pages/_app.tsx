c780b3be1f02bc28c3ed4fafc2669ae3271c0dbf
Jonathon Sherrard <jon@devular.com>
2022-09-27 19:10:49 +0100

    Github Corner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,41 +2,46 @@ import { signOut } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
 import '../styles/mvp.css';
+import GithubCorner from '../components/github-corner';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main>
-      <nav>
-        <Link href="/">Next Auth Credentials Reference</Link>
-        <ul>
-          <li>
-            <Link href="/sign-in">Sign in</Link>
-          </li>
-          <li>
-            <Link href="/register">Register</Link>
-          </li>
-          <li>
-            <Link href="/unprotected-route">Unprotected Route</Link>
-          </li>
-          <li>
-            <Link href="/protected-route">Protected Route</Link>
-          </li>
-          <li>
-            <Link href="/app/dashboard">Protected App</Link>
-          </li>
-          <li>
-            {/* Slightly strange name callbackUrl, i.e where to go after a logout */}
-            <a href="#" onClick={() => signOut({ callbackUrl: '/' })}>
-              Log Out
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <main>
+        <nav>
+          <Link href="/">Next Auth Credentials Reference</Link>
+          <ul>
+            <li>
+              <Link href="/sign-in">Sign in</Link>
+            </li>
+            <li>
+              <Link href="/register">Register</Link>
+            </li>
+            <li>
+              <Link href="/unprotected-route">Unprotected Route</Link>
+            </li>
+            <li>
+              <Link href="/protected-route">Protected Route</Link>
+            </li>
+            <li>
+              <Link href="/app/dashboard">Protected App</Link>
+            </li>
+            <li>
+              {/* Slightly strange name callbackUrl, i.e where to go after a logout */}
+              <a href="#" onClick={() => signOut({ callbackUrl: '/' })}>
+                Log Out
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-      <Component {...pageProps} />
-      <footer>
-        <p>© {new Date().getFullYear()} Devular</p>
-      </footer>
-    </main>
+        <Component {...pageProps} />
+        <GithubCorner href="https://github.com/devular/next-auth-credentials-reference" />
+        <footer>
+          <p>© {new Date().getFullYear()} Devular</p>
+        </footer>
+      </main>
+    </>
   );
 }
 
